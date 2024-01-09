b64410adc8799af88a51cc53a06a767ad8a4d3ce
burillu <burillu90@hotmail.it>
2024-01-09 17:43:18 +0100

    imporve edit form action and input value

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -9,7 +9,7 @@
             <div class="row justify-content-center">
                 <div class="col-8">
                     <h2 class="text-center"> Editing {{ $comic->title }}</h2>
-                    <form action="{{ route('comics.store') }}" method="POST">
+                    <form action="{{ route('comics.update', $comic->id) }}" method="POST">
                         {{-- token --}}
                         @csrf
 
@@ -20,17 +20,23 @@
                         <input id="title" type="text" value="{{ old('title', $comic->title) }}" name="title"
                             class="mb-3 form-control">
                         <label for="description">Description:</label>
-                        <input id="description" type="text" name="description" class="mb-3 form-control">
+                        <input id="description" type="text" value="{{ old('description', $comic->description) }}"
+                            name="description" class="mb-3 form-control">
                         <label for="thumb">Image Link:</label>
-                        <input id="thumb" type="text" name="thumb" class="mb-3 form-control">
+                        <input id="thumb" type="text" value="{{ old('thumb', $comic->thumb) }}" name="thumb"
+                            class="mb-3 form-control">
                         <label for="price">Price:</label>
-                        <input id="price" type="text" name="price" class="mb-3 form-control">
+                        <input id="price" type="text" value="{{ old('price', $comic->price) }}" pricename="price"
+                            class="mb-3 form-control">
                         <label for="sale_date">Sale Date:</label>
-                        <input id="sale_date" type="text" name="sale_date" class="mb-3 form-control">
+                        <input id="sale_date" type="text" value="{{ old('sale_date', $comic->sale_date) }}"
+                            name="sale_date" class="mb-3 form-control">
                         <label for="series">Serie:</label>
-                        <input id="series" type="text" name="series" class="mb-3 form-control">
+                        <input id="series" type="text" value="{{ old('series', $comic->series) }}" name="series"
+                            class="mb-3 form-control">
                         <label for="type">Type:</label>
-                        <input id="type" type="text" name="type" class="mb-3 form-control">
+                        <input id="type" type="text" value="{{ old('type', $comic->type) }}" name="type"
+                            class="mb-3 form-control">
 
                         <button type="submit" class="btn btn-success"><i class="fa-solid fa-plus"></i></button>
                     </form>
